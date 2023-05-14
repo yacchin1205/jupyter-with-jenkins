e8dc2464fcb6ede85b936503e70e7452f1312264
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-05-14 12:13:32 +0900

    Fix jupyter-lab script

---
+++ conf/bin/run-hook.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -xe
+
+run-hooks () {
+    # Source scripts or run executable files in a directory
+    if [[ ! -d "$1" ]] ; then
+        return
+    fi
+    echo "$0: running hooks in $1"
+    for f in "$1/"*; do
+        case "$f" in
+            *.sh)
+                echo "$0: running $f"
+                source "$f"
+                ;;
+            *)
+                if [[ -x "$f" ]] ; then
+                    echo "$0: running $f"
+                    "$f"
+                else
+                    echo "$0: ignoring $f"
+                fi
+                ;;
+        esac
+    done
+    echo "$0: done running hooks in $1"
+}
+
+if [[ -z "${SUPERVISOR_INITIALIZED}" ]]; then
+    run-hooks /usr/local/bin/start-notebook.d
+    run-hooks /usr/local/bin/before-notebook.d
+fi
